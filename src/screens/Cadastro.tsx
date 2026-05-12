978f95105cbd509a5884fe04a98ea58c87feb062
GABRIEL ALVES LOPES <gabriel.alopes4@senacsp.edu.br>
2026-05-11 21:41:02 -0300

    versão final 2

--- src/screens/Cadastro.tsx
+++ src/screens/Cadastro.tsx
@@ -6,22 +6,46 @@ import { auth } from "../lib/firebase";
 export default function Cadastro({ navigation }: any){
 
 const [nome,setNome] = useState("");
-const [telefone,setTelefone] = useState("");
 const [email,setEmail] = useState("");
 const [password,setPassword] = useState("");
+const [confirmPassword,setConfirmPassword] = useState("");
 
 async function handleRegister(){
+
+  if(!nome || !email || !password || !confirmPassword){
+    Alert.alert("Erro","Preencha todos os campos");
+    return;
+  }
+
+  if(password !== confirmPassword){
+    Alert.alert("Erro","As senhas não coincidem");
+    return;
+  }
+
   try{
+
     const create = await createUserWithEmailAndPassword(auth,email,password);
+
     console.log("REGISTER OK", create.user.uid);
 
     Alert.alert("Sucesso","Conta criada com sucesso!");
 
     navigation.navigate("Login");
 
-  }catch(e){
-    console.log("REGISTER FAIL",e);
-    Alert.alert("Erro","Não foi possível criar a conta");
+  }catch(e:any){
+
+    console.log("REGISTER FAIL", e.code);
+
+    if(e.code === "auth/email-already-in-use"){
+      Alert.alert("Erro","Esse email já está cadastrado");
+    }else if(e.code === "auth/invalid-email"){
+      Alert.alert("Erro","Email inválido");
+    }else if(e.code === "auth/weak-password"){
+      Alert.alert("Erro","A senha precisa ter pelo menos 6 caracteres");
+    }else{
+      Alert.alert("Erro","Não foi possível criar a conta");
+    }
+
   }
 }
 
@@ -29,8 +53,14 @@ return(
 
 <View style={{flex:1, justifyContent:"center", padding:20, gap:12, backgroundColor:"#fff"}}>
 
-<Text style={{fontSize:22, color:"#1565C0", textAlign:"center", marginBottom:10, fontWeight:"bold"}}>
-CADASTRO
+<Text style={{
+fontSize:32,
+color:"#1565C0",
+textAlign:"center",
+marginBottom:10,
+fontWeight:"bold"
+}}>
+CineHub
 </Text>
 
 <TextInput
@@ -38,22 +68,6 @@ placeholder="Nome"
 placeholderTextColor="#777"
 value={nome}
 onChangeText={setNome}
-style={{
-borderWidth:1,
-borderColor:"#1565C0",
-padding:12,
-borderRadius:8,
-color:"#000",
-backgroundColor:"#F5F5F5"
-}}
-/>
-
-<TextInput
-placeholder="Telefone"
-placeholderTextColor="#777"
-value={telefone}
-onChangeText={setTelefone}
-keyboardType="phone-pad"
 style={{
 borderWidth:1,
 borderColor:"#1565C0",
@@ -86,6 +100,22 @@ placeholderTextColor="#777"
 secureTextEntry
 value={password}
 onChangeText={setPassword}
+style={{
+borderWidth:1,
+borderColor:"#1565C0",
+padding:12,
+borderRadius:8,
+color:"#000",
+backgroundColor:"#F5F5F5"
+}}
+/>
+
+<TextInput
+placeholder="Confirmar senha"
+placeholderTextColor="#777"
+secureTextEntry
+value={confirmPassword}
+onChangeText={setConfirmPassword}
 style={{
 borderWidth:1,
 borderColor:"#1565C0",
